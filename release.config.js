8491273145004846ea0f4617e6e4629dbde808e5
Karla Ramirez <169180444+KarlaVirtual@users.noreply.github.com>
2025-09-22 10:09:38 -0500

    Create release.config.js

---
+++ release.config.js
@@ -0,0 +1,25 @@
+const config = {
+  branches: ['main'],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        parserOpts: {
+          headerPattern: /^(\w+):"(.*)"$/,
+          headerCorrespondence: ['type', 'subject']
+        }
+      }
+    ],
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      }
+    ],
+    '@semantic-release/github'
+  ]
+};
+
+module.exports = config;
